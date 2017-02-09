0cd6208ae7bcbacae20d1c13b33cbb9cad02e0c6
Alistair Munn <alistairmunn@btinternet.com>
2017-02-09 17:33:03 +0000

    Completed chapter 9.3 on password validators

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -87,6 +87,11 @@ DATABASES = {
     }
 }
 
+# Password Hashers
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
@@ -97,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': { 'min_length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
